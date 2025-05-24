eb1c0a9f72be3400a9ffdbe8dbf3585eb0ed253b
Quillraven <quillraven@gmail.com>
2025-05-24 17:04:57 +0200

    add collision to basic tiles

--- assets/maps/tileset.tsx
+++ assets/maps/tileset.tsx
@@ -1,7 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="tileset" tilewidth="32" tileheight="32" spacing="4" margin="2" tilecount="98" columns="14">
  <image source="tileset.png" width="512" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="4.25" y="5.5" width="24.25" height="23.25">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
   <animation>
    <frame tileid="46" duration="250"/>
    <frame tileid="47" duration="250"/>
@@ -10,5 +20,70 @@
    <frame tileid="48" duration="250"/>
    <frame tileid="47" duration="250"/>
   </animation>
+ </tile>
+ <tile id="47">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="48">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="50">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="51">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="52">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="53">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="31" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="31" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="56">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="30" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="30" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
  </tile>
 </tileset>
