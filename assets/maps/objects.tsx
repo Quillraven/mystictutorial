f9767d2695abf67f3641708232678b8ee15197bd
Quillraven <quillraven@gmail.com>
2025-05-24 16:44:52 +0200

    add hero object to tiledmap

--- assets/maps/objects.tsx
+++ assets/maps/objects.tsx
@@ -1,2 +1,21 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="objects" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="objects" tilewidth="64" tileheight="64" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="speed" type="float" value="5"/>
+  </properties>
+  <image source="objects/hero.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" name="collission" x="19.75" y="53.5" width="24.25" height="10.5">
+    <ellipse/>
+   </object>
+   <object id="3" name="interact" x="0" y="8" width="64" height="64">
+    <properties>
+     <property name="sensor" type="bool" value="true"/>
+    </properties>
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
