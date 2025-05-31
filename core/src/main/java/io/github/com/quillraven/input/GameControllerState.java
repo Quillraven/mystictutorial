c25da5633d3b6e86813f19efb4bd83c850856002
Quillraven <quillraven@gmail.com>
2025-05-31 20:30:40 +0200

    refactor game controller move

--- core/src/main/java/io/github/com/quillraven/input/GameControllerState.java
+++ core/src/main/java/io/github/com/quillraven/input/GameControllerState.java
@@ -12,81 +12,33 @@ public class GameControllerState implements ControllerState {
         this.controllerEntities = controllerEntities;
     }
 
+    private void moveEntities(float dx, float dy) {
+        for (Entity entity : controllerEntities) {
+            Move move = Move.MAPPER.get(entity);
+            if (move != null) {
+                move.getDirection().x += dx;
+                move.getDirection().y += dy;
+            }
+        }
+    }
+
     @Override
     public void keyDown(Command command) {
         switch (command) {
-            case UP:
-                for (Entity entity : controllerEntities) {
-                    Move move = Move.MAPPER.get(entity);
-                    if (move != null) {
-                        move.getDirection().y += 1f;
-                    }
-                }
-                break;
-            case DOWN:
-                for (Entity entity : controllerEntities) {
-                    Move move = Move.MAPPER.get(entity);
-                    if (move != null) {
-                        move.getDirection().y -= 1f;
-                    }
-                }
-                break;
-            case LEFT:
-                for (Entity entity : controllerEntities) {
-                    Move move = Move.MAPPER.get(entity);
-                    if (move != null) {
-                        move.getDirection().x -= 1f;
-                    }
-                }
-                break;
-            case RIGHT:
-                for (Entity entity : controllerEntities) {
-                    Move move = Move.MAPPER.get(entity);
-                    if (move != null) {
-                        move.getDirection().x += 1f;
-                    }
-                }
-                break;
-            default:
+            case UP -> moveEntities(0f, 1f);
+            case DOWN -> moveEntities(0f, -1f);
+            case LEFT -> moveEntities(-1f, 0f);
+            case RIGHT -> moveEntities(1f, 0f);
         }
     }
 
     @Override
     public void keyUp(Command command) {
         switch (command) {
-            case UP:
-                for (Entity entity : controllerEntities) {
-                    Move move = Move.MAPPER.get(entity);
-                    if (move != null) {
-                        move.getDirection().y -= 1f;
-                    }
-                }
-                break;
-            case DOWN:
-                for (Entity entity : controllerEntities) {
-                    Move move = Move.MAPPER.get(entity);
-                    if (move != null) {
-                        move.getDirection().y += 1f;
-                    }
-                }
-                break;
-            case LEFT:
-                for (Entity entity : controllerEntities) {
-                    Move move = Move.MAPPER.get(entity);
-                    if (move != null) {
-                        move.getDirection().x += 1f;
-                    }
-                }
-                break;
-            case RIGHT:
-                for (Entity entity : controllerEntities) {
-                    Move move = Move.MAPPER.get(entity);
-                    if (move != null) {
-                        move.getDirection().x -= 1f;
-                    }
-                }
-                break;
-            default:
+            case UP -> moveEntities(0f, -1f);
+            case DOWN -> moveEntities(0f, 1f);
+            case LEFT -> moveEntities(1f, 0f);
+            case RIGHT -> moveEntities(-1f, 0f);
         }
     }
 }
